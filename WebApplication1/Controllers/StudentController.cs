77095b50fcb84c035b585523b712dbe003578847
Sabenee-Stha <sabinsthaa10@gmail.com>
2025-08-10 17:06:33 +1000

    Update StudentController.cs

--- WebApplication1/Controllers/StudentController.cs
+++ WebApplication1/Controllers/StudentController.cs
@@ -16,7 +16,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult Welcome(string name, int numTimes = 1)
         {
-            ViewData["Message"] = "Hello and Welcome " + name;
+            ViewData["Message"] = "Hello and Welcome to our page " + name;
             // This is my first change for the GitHub assignment.
             ViewData["NumTimes"] = numTimes;
 
