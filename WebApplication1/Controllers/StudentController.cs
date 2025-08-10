5d00467df42d17845c757ac4a6f895b5ade0f953
Sabenee-Stha <sabinsthaa10@gmail.com>
2025-08-10 11:27:02 +1000

    Initial commit

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello and  " + name;
+            // This is my first change for the GitHub assignment.
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
